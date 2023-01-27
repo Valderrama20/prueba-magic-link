c3c6a91c3ca84eabe21a485b03e2d49794d4d4e5
Valderrama20 <jose23122009@gmail.com>
2023-01-26 22:14:03 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -15,37 +15,51 @@ import './App.css';
 function App() {
   const m = new Magic('pk_live_5AAEAC4E3D514B5B'); // ✨
 
+  const magic = new Magic('YOUR_API_KEY', {
+    extensions: [new OAuthExtension()],
+  });
 
-  const data = async () => {
-    const isLoggedIn = await m.user.isLoggedIn();
-    if(isLoggedIn) {
-      const userMetadata = await m.user.getMetadata();
-      console.log(userMetadata)
-    }
-    else alert("No hay usuario")
-    
-
+  const twitter = async () => {
+    await magic.oauth.loginWithRedirect({
+      provider: 'twitter' /* 'google', 'facebook', 'apple', or 'github' */,
+      redirectURI: 'https://auth.magic.link/v1/oauth2/y7sxrJKbPDnSvfcRfAZoTPu7vJwveTcDVUVwa1NPrUQ=/callback',
+      // scope: ['user:email'] /* optional */,
+    });
+    const result = await magic.oauth.getRedirectResult();
+    console.log(result)
   }
 
-  const render = async () => {
-       
-  };
-
-  const Login = async e => {
-    e.preventDefault()
-    await m.auth.loginWithMagicLink({ email: 'jose2312200226@gmail.com' });
+  // const data = async () => {
+  //   const isLoggedIn = await m.user.isLoggedIn();
+  //   if(isLoggedIn) {
+  //     const userMetadata = await m.user.getMetadata();
+  //     console.log(userMetadata)
+  //   }
+  //   else alert("No hay usuario")
     
-  };
 
-  const Logout = async () => {
-    await m.user.logout()
-  };
+  // }
+
+  // const render = async () => {
+       
+  // };
+
+  // const Login = async e => {
+  //   e.preventDefault()
+  //   await m.auth.loginWithMagicLink({ email: 'jose2312200226@gmail.com' });
+    
+  // };
+
+  // const Logout = async () => {
+  //   await m.user.logout()
+  // };
 
   return (
     <div className="App">
-     {<button onClick={(e)=>Login(e)}>login</button>}
+     {/* {<button onClick={(e)=>Login(e)}>login</button>}
      {<button onClick={()=>data()}>data</button>}
-    {<button onClick={()=> Logout()}>logout</button>}
+    {<button onClick={()=> Logout()}>logout</button>} */}
+    {<button onClick={()=> twitter() }>twitter</button>}
     </div>
   );
 }
